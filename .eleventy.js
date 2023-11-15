4ba54a212adf0bde72040d6b6f50eb9113a01025
Cody <codywscw@gmail.com>
2023-11-15 02:05:37 -0600

    add html min to build

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
 	eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
 	eleventyConfig.addPassthroughCopy({ './src/sitemap.xml': '/sitemap.xml' });
-
+	eleventyConfig.addTransform('htmlmin', htmlMinTransform);
 	// normally, 11ty will render dates on blog posts in full JSDate format (Fri Dec 02 18:00:00 GMT-0600)
 	// this filter allows dates to be converted into a normal, locale format. view the docs to learn more (https://moment.github.io/luxon/api-docs/index.html#datetime)
 	eleventyConfig.addFilter('postDate', (dateObj) => {
